9b6d97f21c21816aedf60ed2ad57f51e5b2119c2
Yulissa Alvarez <yulissa.alvarez@cetys.edu.mx>
2024-05-18 14:53:44 -0700

    implementación de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LOGINComponent } from './AUTH/LOGIN/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '',
+        title: 'Iniciar sesión',
+        pathMatch: 'full',
+        component: LOGINComponent
+    }
+];
